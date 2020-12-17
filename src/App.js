67925753ef5dad63e4d979850e402bb5a9e5d977
adyarabiat <ady.arabiat@gmail.com>
2020-12-17 21:51:42 +0400

    fetching the data from the api commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import Header from "./components/ui/Header";
+
+import "./app.css";
 
 const App = () => {
+    const [items, setItems] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const fetch = axios(
+            `https://www.breakingbadapi.com/api/characters`
+        ).then((res) => {
+            console.log(res.data);
+        });
+    }, []);
+
     return (
         <div className="container">
-            <h1>Hello World</h1>
+            <Header />
         </div>
     );
 };
